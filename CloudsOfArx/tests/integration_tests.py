2c2e3561396f8a9ad8b9b188160b1d3654d0ee1c
James Sunseri <jamessunseri@Jamess-MacBook-Pro.local>
2023-01-05 13:49:35 -0800

    raising coverage

--- CloudsOfArx/tests/integration_tests.py
+++ CloudsOfArx/tests/integration_tests.py
@@ -3,24 +3,17 @@ import os
 
 
 def test_secret_token():
-    try:
-        token = os.environ.get("ADS_KEY")
-        if token[0] == 'z':
-            assert True
-        else:
-            assert False
-
-    except:
+    token = os.environ.get("ADS_KEY")
+    if token[0] == 'z':
+        assert True
+    else:
         assert False
     
 
 def test_wordcloud():
-    try:
-        ADS_API_KEY = os.environ.get("ADS_KEY")
-        flag = CloudsOfArx.create_wordcloud(ADS_API_KEY, 'Sunseri, James', test_case=True)
-        if flag == True:
-            assert True
-        else:
-            assert False
-    except:
+    ADS_API_KEY = os.environ.get("ADS_KEY")
+    flag = CloudsOfArx.create_wordcloud(ADS_API_KEY, 'Sunseri, James', test_case=True)
+    if flag == True:
+        assert True
+    else:
         assert False
--- CloudsOfArx/tests/unit_tests.py
+++ CloudsOfArx/tests/unit_tests.py
@@ -4,24 +4,18 @@ import CloudsOfArx.main as CloudsOfArx
 import os
 
 def test_tprint():
-    try:
-        if CloudsOfArx.test_print() == True:
-            assert True
-        else:
-            assert False
-    except:
+    if CloudsOfArx.test_print() == True:
+        assert True
+    else:
         assert False
 
 def test_clipping_mask():
-    try:
-        test_dir_path = os.path.realpath(__file__)[:-13]
-        image_file = test_dir_path + 'BlackHole_Example.png'
-        mask, image = create_image.create_image_mask(image_file)
+    test_dir_path = os.path.realpath(__file__)[:-13]
+    image_file = test_dir_path + 'BlackHole_Example.png'
+    mask, image = create_image.create_image_mask(image_file)
 
-        if mask.shape == image.shape:
-            assert True
-        else:
-            assert False
-    except:
+    if mask.shape == image.shape:
+        assert True
+    else:
         assert False
 
