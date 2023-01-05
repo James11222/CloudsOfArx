6ce46ff9434c2e591cc4b806dbb194c87b18aa40
James Sunseri <jamessunseri@Jamess-MacBook-Pro.local>
2023-01-05 13:44:33 -0800

    fixed again

--- CloudsOfArx/tests/unit_tests.py
+++ CloudsOfArx/tests/unit_tests.py
@@ -14,11 +14,14 @@ def test_tprint():
 
 def test_clipping_mask():
     try:
-        image_dir = os.getcwd() + "/../examples/BlackHole_Example.png"
-        mask, image = create_image.create_image_mask(image_dir)
+        test_dir_path = os.path.realpath(__file__)[:-13]
+        image_file = test_dir_path + 'BlackHole_Example.png'
+        mask, image = create_image.create_image_mask(image_file)
+
         if mask.shape == image.shape:
             assert True
         else:
             assert False
     except:
         assert False
+
